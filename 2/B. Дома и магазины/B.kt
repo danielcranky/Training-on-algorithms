7192b31cd1373946d6953c02cf5d1b157bc662dc
danielcranky <danielcranky@gmail.com>
2021-09-04 02:48:33 +0300

    Create B.kt

---
+++ 2/B. Дома и магазины/B.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val array = readLine()!!.trim().split(" ").map { it.toInt() }
+    val arrayOfIndex2: MutableList<Int> = mutableListOf()
+    val arrayOfIndex1: MutableList<Int> = mutableListOf()
+    val minFromMax: MutableList<Int> = mutableListOf()
+
+    for ((index, num) in array.withIndex()) {
+        if (num == 2) {
+            arrayOfIndex2.add(index)
+        }
+    }
+
+    for ((index, num) in array.withIndex()) {
+        if (num == 1) {
+            arrayOfIndex1.add(index)
+        }
+    }
+
+    for (num1 in arrayOfIndex1) {
+        val arrayOfMin: MutableList<Int> = mutableListOf()
+        var min = array.size
+
+        for (num2 in arrayOfIndex2) {
+            if (num1 >= num2) {
+                if (num1 - num2 < min) {
+                    min = num1 - num2
+                    arrayOfMin.add(min)
+                }
+            } else {
+                if (num2 - num1 < min) {
+                    min = num2 - num1
+                    arrayOfMin.add(min)
+                }
+            }
+        }
+
+        minFromMax.add(arrayOfMin.min()!!)
+    }
+
+    println(minFromMax.max())
+
+}
